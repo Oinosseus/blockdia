5242156dd15a45edc3500ee25a3b00c28de482f9
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2017-07-25 21:46:42 +0200

    save/restore main window

--- src/blockeditor/main.cpp
+++ src/blockeditor/main.cpp
@@ -1,8 +1,13 @@
 #include "mainwindow.h"
+#include <QCoreApplication>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setOrganizationName("Oinosseus");
+    QCoreApplication::setOrganizationDomain("https://github.com/Oinosseus/blockdia");
+    QCoreApplication::setApplicationName("Block Editor");
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
--- src/blockeditor/mainwindow.cpp
+++ src/blockeditor/mainwindow.cpp
@@ -1,11 +1,36 @@
 #include "mainwindow.h"
 
+#include <QLabel>
+#include <QDockWidget>
+#include <QSettings>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    // central widget
+    this->widgetMain = new QTabWidget(this);
+    this->setCentralWidget(this->widgetMain);
+
+    // block browser dock
+    QDockWidget *dock = new QDockWidget("Block Browser", this);
+    dock->setObjectName("BlockBrowser");
+    dock->setAllowedAreas(Qt::LeftDockWidgetArea);
+    dock->setWidget(new QLabel("Hello World"));
+    dock->setFloating(false);
+    dock->setFeatures(QDockWidget::NoDockWidgetFeatures);
+    this->addDockWidget(Qt::LeftDockWidgetArea, dock);
+
+    // restore window state
+    QSettings s;
+    this->restoreState(s.value("Mainwindow/State").toByteArray());
+    this->restoreGeometry(s.value("MainWindow/Geometry").toByteArray());
+
 }
 
 MainWindow::~MainWindow()
 {
-
+    // save window state
+    QSettings s;
+    s.setValue("MainWindow/State", this->saveState());
+    s.setValue("MainWindow/Geometry", this->saveGeometry());
 }
