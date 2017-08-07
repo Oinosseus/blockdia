986c0fd0035948eb4a696fe0162419fda82cecf6
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2017-08-07 20:49:20 +0200

    creating documentation for namespace libblockdia

--- include/libblockdia.h
+++ include/libblockdia.h
@@ -3,6 +3,24 @@
 
 // this can be used as central include file users of the library
 
+/**
+ * @brief A library for managing block diagrams
+ *
+ * The data is stored in Block objects.
+ * Blocks can be adjusted with Parameter objects.
+ * There are different types of parameters available (int, string, enum).
+ * Blocks can be connected together by their Input and Output objects.
+ *
+ * The library contains graphic views and edit widgets to show and edit blocks and block diagrams.
+ *
+ * The Block data hierarchy looks like this:
+ * @image html BlockHierarchy.svg "Block data hierarchy" width=200px
+ *
+ */
+namespace libblockdia {
+
+}
+
 
 // block data classes
 #include <input.h>
